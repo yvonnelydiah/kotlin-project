b21e69599a02d45c16745742e8b35cb7ab4bc8e0
Queenvinemuthoni <queenvinemuthoni@gmail.com>
2023-04-19 08:52:59 -0400

    kotlin

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,24 @@
+fun main(args: Array<String>) {
+    var name = "Nyabs" //string
+    var age = 18 //int
+    println(name)
+    println(age)
+
+    var a = 70
+    var b = 65
+    var sum = a + b
+    println(sum)
+
+
+    var c = 100
+    var d = 50
+    var subtract = c - d
+    println(subtract)
+
+
+    var e = 500
+    var f = 4
+    var multiply = e * f
+    println(multiply)
+
+    }
